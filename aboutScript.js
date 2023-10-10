944b9cb92ab9557b460293d93fb81111a7d8e891
satyam-sm <123965283+satyam-sm@users.noreply.github.com>
2023-10-10 08:41:58 +0530

    Final prateek files 9th oct

---
+++ aboutScript.js
@@ -0,0 +1,11 @@
+let message = document.getElementById('message');
+
+function showMessage(){
+    message.style.display = "block";
+}
+
+let btn = document.getElementById('btn');
+
+function hideMessage(){
+    message.style.display = "none";
+}
